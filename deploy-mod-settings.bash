f3da84ece7fd07110a63fbd58a99793c611c0a84
gaunthan <gaunthanhuang@foxmail.com>
2019-05-30 09:14:10 +0800

    Clear old setting

--- deploy-mod-settings.bash
+++ deploy-mod-settings.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-mods_setup_file="~/serverfiles/mods/dedicated_server_mods_setup.lua"
-mods_setting_file="~/serverfiles/mods/modsettings.lua"
+SERVER_NAME="dstserver"
+MODS_SETUP_FILE="~/serverfiles/mods/dedicated_server_mods_setup.lua"
+MODS_SETTING_FILE="~/serverfiles/mods/modsettings.lua"
 
 touch /tmp/mods_setup_file && > /tmp/mods_setup_file
 touch /tmp/mods_setting_file && > /tmp/mods_setting_file
@@ -14,4 +15,8 @@ do
   echo "ForceEnableMod(\"workshop-${mod_id}\")" >> /tmp/mods_setting_file
 done
 
-su -s /bin/bash -c "cat /tmp/mods_setup_file >> $mods_setup_file; cat /tmp/mods_setting_file >> $mods_setting_file" dstserver
+echo "Enter information for $SERVER_NAME:"
+su -s /bin/bash -c "sed -i '/^ServerModSetup/d' $MODS_SETUP_FILE; \
+                    sed -i '/^ForceEnableMod/d' $MODS_SETTING_FILE; \
+                    cat /tmp/mods_setup_file >> $MODS_SETUP_FILE; \
+                    cat /tmp/mods_setting_file >> $MODS_SETTING_FILE" $SERVER_NAME
