b7eca46027c1af47be0400171021e4ada0158715
gaunthan <gaunthanhuang@foxmail.com>
2019-05-30 09:55:10 +0800

    Script for easily installing DST game server

---
+++ easy-install.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# 1. Clone this repo
+cd /tmp
+if [[ -d "/tmp/Dont-Starve-Together-Game-Server" ]]; then
+  cd Dont-Starve-Together-Game-Server
+  git pull
+else
+  git clone https://github.com/gaunthan/Dont-Starve-Together-Game-Server.git
+  cd Dont-Starve-Together-Game-Server
+fi
+
+# 2. Install dstserver
+./install-dependencies.bash
+./install-server.bash
+
+# 3. Install mods
+./deploy-mod-settings.bash
+
+# 4. Configure the Server
+./configure-server.bash
+
+# 5. Start The Server
+su - dstserver -c "~/dstserver start"
